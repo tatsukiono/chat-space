e8385dd759665a5a60460c40062abd73fb105522
tatsukiono <64731027+tatsukiono@users.noreply.github.com>
2020-05-09 15:57:06 +0900

    Carrierwaveを導入

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,13 @@
 class MessagesController < ApplicationController
   def index
   end
+
+  def create
+    Message.create(message_params)
+  end
+
+  private
+  def
+    params.require(:message).permit(:content, :image)
+  end
 end
